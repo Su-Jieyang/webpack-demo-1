75731d9be80d2928ade8cbcc8867eb61ea54ea37
Su <1535631290@qq.com>
2021-05-15 19:34:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import x from "./x.js";
 import png from "./assets/1.png";
-console.log(png);
 
 const div = document.getElementById("app");
-div.innerHTML = `<img src="${png}">`;
+// div.innerHTML = `<img src="${png}">`;
 
 const button = document.createElement("button");
 div.appendChild(button);
